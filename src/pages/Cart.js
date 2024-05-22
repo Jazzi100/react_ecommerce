a7ca004eccd61950387d82b0f1b3eabaa2f8aba5
JAHANZAIB ORANGZAIB <jahanzaiborangzaib@zdsllc.biz>
2024-05-22 18:43:08 +0400

    create single product detail page

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -17,11 +17,11 @@ import { RiDeleteBin6Line } from "react-icons/ri";
 
 const Cart = () => {
   const navigate = useNavigate();
-  const [carts, setCarts] = useState([]);
+  const [cart, setCart] = useState([]);
   const [total, setTotal] = useState(0);
   const [qty, setQty] = useState();
   const [price, setPrice] = useState(0);
-  const [quantity, setQuantity] = useState(1);
+
   useEffect(() => {
     axios({
       method: "get",
@@ -31,42 +31,97 @@ const Cart = () => {
       url: "http://localhost:5000/api/cart/get-cart",
     })
       .then((response) => {
-        console.log(response.data);
-        setCarts(response.data)
-        // Quantity
-        const quantities = response.data.map(item => item.quantity);
-        console.log("Quantities in response : " + quantities);
-        setQty(quantities);
-        // Price
-        const prices = response.data.map(item => item.product_id.price);
-        console.log("Prices in response : " + prices);
-        // Calculate total price for each item
-        const itemTotalPrices = prices.map((price, index) => price * quantities[index]);
+        console.log("API Response : "+ JSON.stringify(response.data.cart));
+        setCart(response.data.cart)
+      //   // Quantity
+      //   const quantities = response.data.cart.map(item => item.quantity);
+      //   // console.log("Quantities in response : " + quantities);
+      //   setQty(quantities);
+      //   // Price
+      //   const prices = response.data.cart.map(item => item.product_id.price);
+      //   // console.log("Prices in response : " + prices);
+      //   // Calculate total price for each item
+      //   const itemTotalPrices = prices.map((price, index) => price * quantities[index]);
 
-      // Calculate total quantity and total price
-      // const totalQuantity = response.data.reduce((acc, item) => acc + item.quantity, 0);
-      const totalPrice = response.data.reduce((acc, item) => acc + item.quantity * item.product_id.price, 0);
+      // // Calculate total quantity and total price
+      // // const totalQuantity = response.data.reduce((acc, item) => acc + item.quantity, 0);
+      // // const totalPrice = response.data.cart.reduce(
+      // //   (acc, item) => acc + item.quantity * item.product_id.price,
+      // //   0
+      // // );
+
+      // let totalPrice = 0;
+      // let totalQuantity = 0;
+      // response.data.cart.forEach(item => {
+      //   totalPrice += item.product_id.price * item.quantity;
+      //   totalQuantity += item.quantity;
+      // });
+
+      // setTotal(totalPrice);
+      // console.log("Total Price : " + totalPrice);
+      // setQty(totalQuantity);
       
-  
-      setQty(quantities);
-      setPrice(prices);
-      setTotal(totalPrice);
+ 
+      // setQty(quantities);
+      // setPrice(prices);
+
+// Calculate total price
+let totalPrice = 0;
+response.data.cart.forEach(item => {
+    totalPrice += item.product_id.price * item.quantity;
+});
+
+setTotal(totalPrice);
+console.log("Total Price : " + totalPrice);
+
+
+      
       })
       .catch((error) => {
         console.log(error);
       });
   }, []);
 
-  // Handle input field change
-  const handleQuantityChange = (e) => {
-    console.log("gggggg : " + e.target.value);
-    const newQuantity = parseInt(e.target.value, 10); 
-    setQuantity(newQuantity); 
-    console.log("New Quantity :" + newQuantity); // Log the newQuantity directly
-  };
+  
+
+  const handleDecrement = (cart_id) => {
+    console.log("Decrement : " + cart_id);
+    setCart(cart => {
+        const updatedCart = cart.map((item) => 
+            cart_id === item._id ? { ...item, quantity: Math.max(item.quantity - 1, 1) } : item
+        );
+        updateTotal(updatedCart);
+        return updatedCart;
+    });
+};
+
+  const handleIncrement = (cart_id) => {
+    console.log("Increment : " + cart_id);
+    setCart((cart) => {
+        const updatedCart = cart.map((item) => 
+            cart_id === item._id ? { ...item, quantity: Math.min(item.quantity + 1, 10) } : item
+        );
+        updateTotal(updatedCart);
+        return updatedCart;
+    });
+};
+
+const updateTotal = (updatedCart) => {
+    let totalPrice = 0;
+    updatedCart.forEach(item => {
+        totalPrice += item.product_id.price * item.quantity;
+    });
+    setTotal(totalPrice);
+};
+
+  
 
   const handleContinueShopping = () => {
     navigate(`/products`);
+  }
+
+  const handleCheckout = () => {
+    navigate(`/checkout`);
   }
 
 
@@ -83,7 +138,7 @@ const Cart = () => {
       })
         .then((response) => {
           console.log(response.data);
-          setCarts(prevCarts => prevCarts.filter(cart => cart._id !== id)); 
+          setCart(prevCarts => prevCarts.filter(cart => cart._id !== id)); 
         })
         .catch((error) => {
           console.log(error);
@@ -95,7 +150,7 @@ const Cart = () => {
   
 
   const cartdiv = {
-    padding: "20px",
+    padding: "15px",
     fontFamily: "Arial",
     border: "1px solid #c6ccdd80",
     boxShadow: "0px 0px 10px 3px rgba(0, 0, 0, 0.1)",
@@ -105,105 +160,116 @@ const Cart = () => {
     float: "right",
     color: "black",
   };
-  const qtyInput = {
-    width: "50px",
+  
+
+  let cartView = "";
+  if(cart.length > 0){
+    cartView = <Container fluid className="p-3">
+    <Row>
+      <Col sm={8}>
+        {cart.length > 0 ? (
+          cart.map((item) => (
+            
+            <div style={cartdiv}>
+          <Row>
+            <Col sm={2}>
+              
+              <Card.Img
+                variant="top"
+                height="100px"
+                width="80px"
+                src={`http://localhost:5000/${item.product_id.productImage}`}
+              />
+            </Col>
+            <Col sm={8}>
+              <p>
+                {item.product_id.title}
+              
+              </p>
+              <a
+                style={{
+                  fontSize: "15px",
+                  color: "black",
+                  textDecoration: "none",
+                }}
+                
+              >
+                <RiDeleteBin6Line />
+                <span onClick={() => removeCart(item._id)} style={{ cursor: "pointer" }}> Remove</span>
+              </a>
+            </Col>
+            <Col sm={2} className="d-flex flex-column justify-content-between align-items-end">
+              <div style={{ marginBottom: "auto", textAlign: "right" }}>
+                AED <b style={{ fontSize: "25px" }}>{item.product_id.price}</b>
+              </div>
+              <div style={{ marginTop: "auto" }}>
+               
+
+                <div className="input-group">
+                  <Button type="button" className="input-group-text" variant="danger" onClick={() => handleDecrement(item._id)}>-</Button>
+                  <div className="form-control text-center">{item.quantity}</div>
+                  <Button type="button" className="input-group-text" variant="danger" onClick={() => handleIncrement(item._id)}>+</Button>
+                </div>
+               
+              </div>
+            </Col>
+          </Row>
+        </div>
+          ))
+        ) : (
+          <tbody>
+         
+          </tbody>
+        )}
+
+      </Col>
+     
+
+      <Col sm={4}>
+        <div style={cartdiv}>
+          <b style={{ fontSize: "25px" }}>Order Summary</b>
+          <p style={{ color: "#7e859b" }}>
+            SubTotal (items)
+            <span style={checkout_price}>AED {total}</span>
+          </p>
+          <p style={{ color: "#7e859b" }}>
+            Shipping Detail
+            <span style={{ float: "right", color: "#dc3545" }}>
+              <b>Free</b>
+            </span>
+          </p>
+          <hr />
+          <p>
+            <b style={{ fontSize: "25px" }}>Total</b>{" "}
+            <span style={{ color: "#7e859b" }}>(Inclusive of VAT)</span>
+            <span style={checkout_price}>AED {total}</span>
+          </p>
+          <Button variant="danger" className="w-100" size="lg" onClick={handleCheckout}>
+            CHECKOUT
+          </Button>
+        </div>
+      </Col>
+    </Row>
+  </Container>
+  
+  }else{
+    cartView = <Container>
+    <Row className="p-5 d-flex flex-column align-items-center">
+      <h4 align="center" className="mb-5" >Your Cart is Empty</h4>
+      <div className="col-6">
+      <Button variant="outline-danger" onClick={handleContinueShopping} className="w-100">
+        Continue Shopping
+      </Button>
+    </div>
+    </Row>
+  </Container>
     
-  };
+  }
+
   return (
     <>
-      <Container fluid className="p-3">
-        <Row>
-          <h3>Cart</h3>
-          
-          <Col sm={8}>
-            {carts.length > 0 ? (
-              carts.map(({ _id,product_id, quantity }) => (
-                <div style={cartdiv}>
-              <Row>
-                <Col sm={2}>
-                  
-                  <Card.Img
-                    variant="top"
-                    height="150px"
-                    src={`http://localhost:5000/${product_id.productImage}`}
-                  />
-                </Col>
-                <Col sm={8}>
-                  <p>
-                    {product_id.title}
-                  </p>
-                  <a
-                    style={{
-                      fontSize: "15px",
-                      color: "black",
-                      textDecoration: "none",
-                    }}
-                    
-                  >
-                    <RiDeleteBin6Line />
-                    <span onClick={() => removeCart(_id)} style={{ cursor: "pointer" }}> Remove</span>
-                  </a>
-                </Col>
-                <Col sm={2} className="d-flex flex-column justify-content-between align-items-end">
-                  <div style={{ marginBottom: "auto", textAlign: "right" }}>
-                    AED <b style={{ fontSize: "25px" }}>{product_id.price}</b>
-                  </div>
-                  <div style={{ marginTop: "auto" }}>
-                    {/* <input type="number" min="1" value={quantity} style={qtyInput} /> */}
-                    <input
-                      type="number"
-                      min="1"
-                      value={quantity}
-                      style={qtyInput}
-                      onChange={handleQuantityChange} // Bind onChange event handler
-                    />
-                  </div>
-                </Col>
-              </Row>
-            </div>
-              ))
-            ) : (
-              <tbody>
-                <tr>
-                  <td colSpan={5}>
-                    <h4 align="center">Your Cart is Empty</h4>
-                  </td>
-                </tr>
-              </tbody>
-            )}
-
-            <Button className="text-center" variant="outline-danger" onClick={handleContinueShopping}>
-              Continoue Shopping
-            </Button>
-          </Col>
-         
-
-          <Col sm={4}>
-            <div style={cartdiv}>
-              <b style={{ fontSize: "25px" }}>Order Summary</b>
-              <p style={{ color: "#7e859b" }}>
-                SubTotal (items)
-                <span style={checkout_price}>AED {total}</span>
-              </p>
-              <p style={{ color: "#7e859b" }}>
-                Shipping Detail
-                <span style={{ float: "right", color: "#dc3545" }}>
-                  <b>Free </b>
-                </span>
-              </p>
-              <hr />
-              <p>
-                <b style={{ fontSize: "25px" }}>Total</b>{" "}
-                <span style={{ color: "#7e859b" }}>(Inclusive of VAT)</span>
-                <span style={checkout_price}>AED {total}</span>
-              </p>
-              <Button variant="danger" className="w-100" size="lg">
-                CHECKOUT
-              </Button>
-            </div>
-          </Col>
-        </Row>
-      </Container>
+    <h3>Cart</h3>
+      {cartView}
     </>
   );
 };
--- src/pages/ProductDetail.js
+++ src/pages/ProductDetail.js
@@ -1,14 +1,16 @@
 import React, { useState, useEffect } from "react";
-import {Row,Col,Container } from "react-bootstrap";
+import {Row,Col,Container, Card, Button } from "react-bootstrap";
 import axios from "axios";
 import { useParams } from 'react-router-dom';
 
 import Categories from "./Categories";
+import "./productDetails.css";
 
 function ProductDetail (){
     const [product, setProduct] = useState([]);
     const { id } = useParams();
 
+    
     
     useEffect(() => {
         axios({
@@ -20,12 +22,22 @@ function ProductDetail (){
         })
           .then((response) => {
             console.log(response.data);
-            setProduct(response.data);
+            setProduct(response.data.product);
           })
           .catch((error) => {
             console.log(error);
           });
       }, []);
+
+      const divStyle = {
+        padding: '10px',
+        borderRadius: '5px',
+        boxShadow: '0 0 20px rgba(0, 0, 0, 0.2)',
+        margin: '10px'
+      };
+
+      
+
     return (
         <Container>
         <Row>
@@ -36,8 +48,54 @@ function ProductDetail (){
             <Categories />
           </Col>
           <Col sm={10}>
-            <Row>
+            <Row style={divStyle}>
+              <Col sm={6}>
+                <p>
+                  <Card.Img
+                    variant="top"
+                    height="400px"
+                    width="50%"
+                    src={`http://localhost:5000/${product.productImage}`}
+                  />
+                </p>
               
+              </Col>
+              <Col sm={6}>
+              <div class="details">
+						<h3 class="product-title">{product.title}</h3>
+						<div class="rating">
+							<div class="stars">
+								<span class="fa fa-star checked"></span>
+								<span class="fa fa-star checked"></span>
+								<span class="fa fa-star checked"></span>
+								<span class="fa fa-star"></span>
+								<span class="fa fa-star"></span>
+							</div>
+							<span class="review-no">41 reviews</span>
+						</div>
+						<h4 class="price">current price: <span>AED {product.price}</span></h4>
+            <p class="product-description">{product.description}</p>
+						
+						{/* <p class="vote"><strong>91%</strong> of buyers enjoyed this product! <strong>(87 votes)</strong></p>
+						<h5 class="sizes">sizes:
+							<span class="size" data-toggle="tooltip" title="small">s</span>
+							<span class="size" data-toggle="tooltip" title="medium">m</span>
+							<span class="size" data-toggle="tooltip" title="large">l</span>
+							<span class="size" data-toggle="tooltip" title="xtra large">xl</span>
+						</h5>
+						<h5 class="colors">colors:
+							<span class="color orange not-available" data-toggle="tooltip" title="Not In store"></span>
+							<span class="color green"></span>
+							<span class="color blue"></span>
+						</h5> */}
+						<div class="action">
+            <Button variant="danger" className="w-100 addToCart" size="lg">
+            ADD TO CART
+          </Button>
+							
+						</div>
+					</div>
+              </Col>
             </Row>
           </Col>
         </Row>
